6184cd7d6f1d63e475998fb692bf2f236b26d3af
Lee Lazarecky <leelazarecky@gmail.com>
2021-05-23 21:25:33 -0400

    Coded up initial travel app main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      HELLO afaf!
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <div className="container">
+                <h1>Welcome to Travel App!</h1>
+                <h3>My Trips</h3>
+                <button className="add-trip-button">Add Trip</button>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
