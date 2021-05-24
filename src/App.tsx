d182f90059f11fcb8305ea18542016a1735b0a56
Lee Lazarecky <leelazarecky@gmail.com>
2021-05-23 21:41:43 -0400

    Routes the 'Add Trip' button to '/map'

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
 
 function App() {
     return (
-        <div className="App">
 
-            <div className="container">
-                <h1>Welcome to Travel App!</h1>
-                <h3>My Trips</h3>
-                <button className="add-trip-button">Add Trip</button>
-            </div>
+        <Router>
+            <Switch>
+                <Route exact path="/">
 
-        </div>
+                    <div className="App">
+
+                        <div className="container">
+                            <h1>Welcome to Travel App!</h1>
+                            <h3>My Trips</h3>
+
+                            <Link to="/map">
+                                <button className="add-trip-button">Add Trip</button>
+                            </Link>
+                        </div>
+
+                    </div>
+                </Route>
+
+                <Route exact path="/map">
+                    <div>MAP IM THE MAP</div>
+                </Route>
+
+            </Switch>
+        </Router>
+
     );
 }
 
